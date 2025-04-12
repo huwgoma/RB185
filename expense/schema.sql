060f34131b31bb5627d4e9118563e4cb741584b0
Hugo Ma <huwgoma@gmail.com>
2025-04-12 19:26:38 -0400

    Add ability to delete all expenses after confirmation

--- expense/schema.sql
+++ expense/schema.sql
@@ -10,4 +10,5 @@ CREATE TABLE expenses (
 INSERT INTO expenses (amount, memo, created_on)
 VALUES (14.56, 'Pencils', '2025-04-09'),
        (3.29, 'Coffee', DEFAULT),
-       (49.99, 'Text Editor', '2020-03-21');
+       (49.99, 'Text Editor', '2020-03-21'),
+       (300.00, 'Annie''s Present', '2025-04-12');
