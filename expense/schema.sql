01457121930351cc3b5030870fe508dc5e95cce2
Hugo Ma <huwgoma@gmail.com>
2025-04-13 13:59:37 -0400

    Refactor #display_columns into separate calculation methods for #column_header and #horizontal_divider

--- expense/schema.sql
+++ expense/schema.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS expenses;
 
 CREATE TABLE expenses (
   id serial PRIMARY KEY,
+  created_on date NOT NULL DEFAULT NOW(),
   amount money NOT NULL CHECK(amount::numeric > 0.00),
-  memo varchar(255) NOT NULL,
-  created_on date NOT NULL DEFAULT NOW()
+  memo varchar(255) NOT NULL
 );
 
-INSERT INTO expenses (amount, memo, created_on)
-VALUES (14.56, 'Pencils', '2025-04-09'),
-       (3.29, 'Coffee', DEFAULT),
-       (49.99, 'Text Editor', '2020-03-21'),
-       (300.00, 'Annie''s Present', '2025-04-12');
+INSERT INTO expenses (created_on, amount, memo)
+VALUES ('2025-04-09', 14.56, 'Pencils'),
+       (DEFAULT, 3.29, 'Coffee'),
+       ('2020-03-21', 49.99, 'Text Editor'),
+       ('2025-04-12', 300.00, 'Annie''s Present');
